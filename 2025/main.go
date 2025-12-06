421412b70d808bb2a14358cf38ece72e9374418a
Arne <arne@zillhardt.com>
2025-12-06 17:33:15 +0100

    Solved puzzle 02 on day 02

--- 2025/main.go
+++ 2025/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Arne-Zillhardt/advent_of_code/2025/day01/01"
 	"github.com/Arne-Zillhardt/advent_of_code/2025/day01/02"
 	"github.com/Arne-Zillhardt/advent_of_code/2025/day02/01"
+	"github.com/Arne-Zillhardt/advent_of_code/2025/day02/02"
 	"github.com/Arne-Zillhardt/advent_of_code/2025/fileInput"
 )
 
@@ -20,6 +21,7 @@ var puzzles = map[string]Puzzle {
 	"day0101": day0101.NewDay0101(),
 	"day0102": day0102.NewDay0102(),
 	"day0201": day0201.NewDay0201(),
+	"day0202": day0202.NewDay0202(),
 }
 
 func main() {
